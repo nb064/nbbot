d88a5460f38332be0100996ea78081de55d492bd
nb064 <nintendobro164@gmail.com>
2026-02-05 11:06:04 -0500

    new fun commands, and updated admin commands.

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 from datetime import datetime
 from PIL import Image
 from io import BytesIO
+import difflib
 
 intents = discord.Intents.default()
 intents.members = True
@@ -24,7 +25,7 @@ with open("resources/games.json", "r") as gamesRes:
 async def on_ready():
     print('Logged on!')
     game = environ["GAME_ACTIVITY"]
-    await client.change_presence(activity=discord.Game(name=game))
+    await client.change_presence(status=discord.Status.idle, activity=discord.Game(name=game))
 
 #Welcome message
 @client.event
@@ -41,7 +42,7 @@ async def help(ctx):
     embed=discord.Embed(title="NBBot Help", description="Here are all the commands for NBBot.", color=discord.Color.blue())
     embed.add_field(name="NBGames Content", value="game: Get information about a game by NBGames.\nplay: Play music from NBGames games!", inline=False)
     embed.add_field(name="Administration", value="ban: Bans a member.\nkick: Kicks a member.\nmute: Mutes a member.\nunmute: Unmutes a member\npurge: Clears an amount of messages in the channel.\nsend: I will send something!", inline=False)
-    embed.add_field(name="Fun", value="8ball: Get an answer from the 8 Ball!\ncoinflip: Flip a coin!\njerry: Become Jerry!\njoke: I'll tell you a joke!\ntranslate: Translate a message!\nzamn: ZAMN!")
+    embed.add_field(name="Fun", value="8ball: Get an answer from the 8 Ball!\ncoinflip: Flip a coin!\njerry: Become Jerry!\njoke: I'll tell you a joke!\nrate: I will rate an item!\nzamn: ZAMN!")
     embed.add_field(name="Math", value="add: Get the sum of two numbers.\nsubtract: Get the difference of two numbers.\nmultiply: Get the product of two numbers.\ndivide: Get the quotient of two numbers.")
     embed.add_field(name="Other", value="help: See all commands.\nmemberinfo: Gets info about a member.\nping: Get latency.\nrandom: Picks between 2 numbers.\nrepeat: I will repeat something!\nserverinfo: Get info about the server.\nuptime: How long has NBBot been up for?", inline=False)
     await ctx.respond(embed=embed)
@@ -49,45 +50,66 @@ async def help(ctx):
 #Ping Command
 @client.slash_command(name = "ping", description = "Get latency.")
 async def ping(ctx):
-    await ctx.respond(f"{ctx.author.mention}, pong! Latency is {0}ms".format(round(client.latency, 1)))
+    await ctx.respond(f"{ctx.author.mention}, pong! Latency is {0}ms.".format(round(client.latency, 1)))
 
 #Ban Command
 @client.slash_command(name = "ban", description = "Ban a member.")
 async def ban(ctx, member: discord.Member, reason = None):
+
+    #First, check if the member is higher than the bot.
+    if member.top_role >= ctx.guild.me.top_role:
+        #Throws error if bot can't ban the specified member.
+        await ctx.respond(f"{ctx.author.mention}, I don't have the ability to ban this member.", delete_after=3, ephemeral=True)
+        return
+
     #Checks if the author has permission to ban members.
     if ctx.author.guild_permissions.ban_members:
         await member.ban(reason=reason)
-        await ctx.respond(f"{ctx.author.mention}, banned @{member.mention} successfully for {reason}.", delete_after=3)
+        await ctx.respond(f"{ctx.author.mention}, banned @{member.mention} successfully for {reason}.", delete_after=3, ephemeral=True)
     else:
         #Throws error if author doesn't have permission.
-        await ctx.respond(f"{ctx.author.mention}, you don't have permission to use this command.", delete_after=3)
+        await ctx.respond(f"{ctx.author.mention}, you don't have permission to use this command.", delete_after=3, ephemeral=True)
 
 #Kick Command
 @client.slash_command(name = "kick", description = "Kick a member.")
 async def kick(ctx, member: discord.Member, reason = None):
+
+    #First, check if the member is higher than the bot.
+    if member.top_role >= ctx.guild.me.top_role:
+        #Throws error if bot can't kick the specified member.
+        await ctx.respond(f"{ctx.author.mention}, I don't have the ability to kick this member.", delete_after=3, ephemeral=True)
+        return
+
     #Checks if the author has permission to kick members.
     if ctx.author.guild_permissions.kick_members:
         await member.kick(reason=reason)
-        await ctx.respond(f"{ctx.author.mention}, kicked @{member.mention} successfully for {reason}.", delete_after=3)
+        await ctx.respond(f"{ctx.author.mention}, kicked @{member.mention} successfully for {reason}.", delete_after=3, ephemeral=True)
     else:
         #Throws error if author doesn't have permission.
-        await ctx.respond(f"{ctx.author.mention}, you don't have permission to use this command.", delete_after=3)
+        await ctx.respond(f"{ctx.author.mention}, you don't have permission to use this command.", delete_after=3, ephemeral=True)
 
 #Mute Command
 @client.slash_command(name="mute", description = "Mute a member.")
 async def mute(ctx, member: discord.Member, reason = None):
+
+    #First, check if the member is higher than the bot.
+    if member.top_role >= ctx.guild.me.top_role:
+        #Throws error if bot can't mute the specified member.
+        await ctx.respond(f"{ctx.author.mention}, I don't have the ability to mute this member.", delete_after=3, ephemeral=True)
+        return
+
     #Checks if the author has permission to mute members.
     if ctx.author.guild_permissions.manage_roles:
         #Checks the mentioned user for the ability to send messages.
         if ctx.channel.permissions_for(member).send_messages:
             await ctx.channel.set_permissions(member, send_messages=False)
-            await ctx.respond(f"{ctx.author.mention}, muted {member.mention} successfully for {reason}.", delete_after=3)
+            await ctx.respond(f"{ctx.author.mention}, muted {member.mention} successfully for {reason}.", delete_after=3, ephemeral=True)
         else:
             #Sends error message if the user is already muted/can't send messages.
-            await ctx.respond(f"{ctx.author.mention}, {member.mention} is already muted.", delete_after=3)
+            await ctx.respond(f"{ctx.author.mention}, {member.mention} is already muted.", delete_after=3, ephemeral=True)
     else:
         #Throws error if author doesn't have permission.
-        await ctx.respond(f"{ctx.author.mention}, you don't have permission to use this command.", delete_after=3)
+        await ctx.respond(f"{ctx.author.mention}, you don't have permission to use this command.", delete_after=3, ephemeral=True)
 
 #Unmute Command
 @client.slash_command(name="unmute", description = "Unmute a member.")
@@ -97,13 +119,13 @@ async def mute(ctx, member: discord.Member):
         #Checks the mentioned user for the ability to send messages.
         if not ctx.channel.permissions_for(member).send_messages:
             await ctx.channel.set_permissions(member, send_messages=True)
-            await ctx.respond(f"{ctx.author.mention}, unmuted {member.mention} successfully.", delete_after=3)
+            await ctx.respond(f"{ctx.author.mention}, unmuted {member.mention} successfully.", delete_after=3, ephemeral=True)
         else:
             #Sends error message if the user isn't muted/can send messages.
-            await ctx.respond(f"{ctx.author.mention}, {member.mention} isn't muted.", delete_after=3)
+            await ctx.respond(f"{ctx.author.mention}, {member.mention} isn't muted.", delete_after=3, ephemeral=True)
     else:
         #Throws error if author doesn't have permission.
-        await ctx.respond(f"{ctx.author.mention}, you don't have permission to use this command.", delete_after=3)
+        await ctx.respond(f"{ctx.author.mention}, you don't have permission to use this command.", delete_after=3, ephemeral=True)
 
 #Purge Command
 @client.slash_command(name = "purge", description="Purge the messages of a channel.")
@@ -113,12 +135,12 @@ async def purge(ctx, number):
         try:
             number = int(number)
         except ValueError:
-            await ctx.respond(f'{ctx.author.mention}, enter a valid number.', delete_after=3)
+            await ctx.respond(f'{ctx.author.mention}, enter a valid number.', delete_after=3, ephemeral=True)
         await ctx.channel.purge(limit=number)
-        await ctx.respond(f'{ctx.author.mention}, successfully cleared {str(number)} messages.', delete_after=3)
+        await ctx.respond(f'{ctx.author.mention}, successfully cleared {str(number)} messages.', delete_after=3, ephemeral=True)
     else:
         #Throws error if author doesn't have permission.
-        await ctx.respond(f"{ctx.author.mention}, you don't have permission to use this command.", delete_after=3)
+        await ctx.respond(f"{ctx.author.mention}, you don't have permission to use this command.", delete_after=3, ephemeral=True)
 
 #Random Command
 @client.slash_command(name="random", description="Choose between 2 random numbers.")
@@ -141,15 +163,15 @@ async def repeat(ctx, message):
 async def repeat(ctx, message, channel: discord.TextChannel):
     if ctx.author.guild_permissions.administrator:
         await channel.send(message)
-        await ctx.respond(f'{ctx.author.mention}, I sent your message!', delete_after=3)
+        await ctx.respond(f'{ctx.author.mention}, I sent your message!', delete_after=3, ephemeral=True)
     else:
-        await ctx.respond(f"{ctx.author.mention}, you don't have permission to use this command. Please note that for safety reasons only admins can use this command.", delete_after=3)
+        await ctx.respond(f"{ctx.author.mention}, you don't have permission to use this command. Please note that for safety reasons only admins can use this command.", delete_after=3, ephemeral=True)
 
 #Play command
 @client.slash_command(name="play", description="Play music from NBGames games!")
 async def play(ctx, channel: discord.VoiceChannel, announce: bool):
     if ctx.author.voice is None:
-        await ctx.respond(f"{ctx.author.mention}, you need to be connected to a voice channel to use this command.", delete_after=3)
+        await ctx.respond(f"{ctx.author.mention}, you need to be connected to a voice channel to use this command.", delete_after=3, ephemeral=True)
     else:
         #Check if the bot is in a voice channel.
         inVoiceChannel = ctx.voice_client
@@ -200,7 +222,7 @@ async def disconnect(ctx):
         await vc.disconnect()
         await ctx.respond(f"{ctx.author.mention}, I've disconnected from the voice channel.", delete_after=3)
     else:
-        await ctx.respond(f"{ctx.author.mention}, I'm not connected to a voice channel.", delete_after=3)
+        await ctx.respond(f"{ctx.author.mention}, I'm not connected to a voice channel.", delete_after=3, ephemeral=True)
 
 #Game Command
 @client.slash_command(name="game", description="Get info on a game made by NBGames.")
@@ -208,15 +230,26 @@ async def game(ctx, gamename):
     #Convert the gamename string to be entirely lowercase. Best to do this so it's easier to use in an if statement.
     gamename = gamename.lower()
 
+        # Exact match
     if gamename in games:
-        embed = discord.Embed(title=games[gamename]["title"], color=discord.Color.blue())
-        embed.add_field(name="About", value=games[gamename]["about"], inline=False)
-        embed.add_field(name="Release Date", value=games[gamename]["release_date"], inline=False)
-        embed.add_field(name="More Info", value=games[gamename]["more_info"], inline=False)
-        embed.set_thumbnail(url=games[gamename]["thumbnail"])
-        await ctx.respond(embed=embed)
+        g = games[gamename]
     else:
-        await ctx.respond(f'{ctx.author.mention}, I do not understand which game you are talking about. Did you make a spelling error?', delete_after=3)
+        # Try to find a close match
+        close_matches = difflib.get_close_matches(gamename, games.keys(), n=1, cutoff=0.6)
+        if close_matches:
+            g = games[close_matches[0]]
+        else:
+            return await ctx.respond(
+                f"{ctx.author.mention}, I do not understand which game you are talking about. Did you make a spelling error?",
+                ephemeral=True
+            )
+
+    embed = discord.Embed(title=g["title"], color=discord.Color.blue())
+    embed.add_field(name="About", value=g["about"], inline=False)
+    embed.add_field(name="Release Date", value=g["release_date"], inline=False)
+    embed.add_field(name="More Info", value=f'Download the latest version [here]({g["more_info"]}).', inline=False)
+    embed.set_thumbnail(url=g["thumbnail"])
+    await ctx.respond(embed=embed)
 
 #Add command
 @client.slash_command(name="add", description="Get the sum of two numbers.")
@@ -279,13 +312,39 @@ async def joke(ctx):
     jokes = open(f'{os.getcwd()}\\resources\\jokes.txt', encoding='utf-8').read().splitlines()
     await ctx.respond(f'{ctx.author.mention}, {rand.choice(jokes)}')
 
+#Rate command
+@client.slash_command(name="rate", description="I'll rate something!")
+async def rate(ctx, item: str):
+    rating = rand.randint(1, 10)
+    responses = open(f'{os.getcwd()}\\resources\\ratingresponses.txt', encoding='utf-8').read().splitlines()
+
+    await ctx.respond(f'{ctx.author.mention}, I rate {item} a {rating}/10. {rand.choice(responses)}')
+
+#Rate command
+@client.slash_command(name="touchgrass", description="Tell someone to touch grass!")
+async def touchgrass(ctx, member: discord.Member):
+    await ctx.respond(f'{member.mention}, you have been advised to go touch grass. 🌱')
+
+#Avatar command
+@client.slash_command(name="avatar", description="Grabs a member's avatar.")
+async def avatar(ctx, member: discord.Member):
+    member = member or ctx.author
+
+    embed = discord.Embed(title=f"{member.name}'s Avatar", color=discord.Color.blue())
+    embed.set_image(url=member.display_avatar.url)
+
+    await ctx.respond(embed=embed)
+
 #Memberinfo Command
 @client.slash_command(name="memberinfo", description="Get info about a member.")
 async def userinfo(ctx, member: discord.Member):
-    embed=discord.Embed(title=f'{member}', description=f'{member.id}', color=discord.Color.blue())
+    embed=discord.Embed(title=f'{member.name}', description=f'{member.id}', color=discord.Color.blue())
     embed.set_thumbnail(url=member.display_avatar)
     embed.add_field(name="Registered On", value=f'{member.created_at.strftime("%A, %b %d, %Y")}', inline=True)
     embed.add_field(name="Joined On", value=f'{member.joined_at.strftime("%A, %b %d, %Y")}', inline=True)
+    embed.add_field(name='\u200b', value='\u200b') #Line Break
+    embed.add_field(name="User Activity", value=f'{member.activity}', inline=True)
+    embed.add_field(name="Highest Role", value=f'{member.top_role}', inline=True)
     await ctx.respond(embed=embed)
 
 #Serverinfo Command
@@ -296,10 +355,10 @@ async def userinfo(ctx):
         embed.set_thumbnail(url=ctx.guild.icon)
     embed.add_field(name="Created On", value=f'{ctx.guild.created_at.strftime("%A, %b %d, %Y")}', inline=True)
     embed.add_field(name="Owner", value=f'{ctx.guild.owner.mention}', inline=True)
-    embed.add_field(name='\u200b', value='\u200b')
+    embed.add_field(name="System Channel", value=f'{ctx.guild.system_channel.mention}', inline=True)
+    embed.add_field(name="Default Role", value=f'{ctx.guild.default_role}', inline=True)
     embed.add_field(name="Member Count", value=f'{ctx.guild.member_count}', inline=True)
-    embed.add_field(name="Notification Setting", value=f'{ctx.guild.default_notifications}', inline=True)
-    embed.add_field(name="Large", value=f'{ctx.guild.large}', inline=True)
+    embed.add_field(name="Notification Setting", value=f'{ctx.guild.default_notifications.name.replace("_", " ").title()}', inline=True)
     await ctx.respond(embed=embed)
 
 #Jerry image command
